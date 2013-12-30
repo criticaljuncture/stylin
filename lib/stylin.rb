ef7c586a04f2968fd5aa50a9b4f40181715ca39e
Peregrinator <bob.burbach@gmail.com>
2013-12-29 22:18:34 -0500

    Move app_configuration to when the app loads

--- lib/stylin.rb
+++ lib/stylin.rb
@@ -5,24 +5,4 @@ require 'kss'
 require "stylin/engine"
 
 module Stylin
-  config = {
-    stylesheets_path: File.join('app', 'assets', 'stylesheets'),
-    html_styleguides_path: File.join('app', 'styleguides'),
-    section_groupings: nil
-  }
-
-  if File.exists?( File.join('config', 'stylin.yml') )
-    user_config = HashWithIndifferentAccess.new(
-      YAML.load(
-        File.read( File.expand_path(File.join('config', 'stylin.yml'), Rails.root) )
-      )
-    ).symbolize_keys
-  else
-    user_config = {}
-  end
-
-  APP_CONFIG = config.merge( user_config )
-
-  APP_CONFIG[:stylesheets_path] = File.expand_path(APP_CONFIG[:stylesheets_path], Rails.root)
-  APP_CONFIG[:html_styleguides_path] = File.expand_path(APP_CONFIG[:html_styleguides_path], Rails.root)
 end
--- lib/stylin/engine.rb
+++ lib/stylin/engine.rb
@@ -2,6 +2,29 @@ module Stylin
   class Engine < ::Rails::Engine
     isolate_namespace Stylin
 
+    initializer "my_engine.load_app_root" do |app|
+      config = {
+        stylesheets_path: File.join('app', 'assets', 'stylesheets'),
+        html_styleguides_path: File.join('app', 'styleguides'),
+        section_groupings: nil
+      }
+
+      if File.exists?( File.expand_path(File.join('config', 'stylin.yml'), app.root) )
+        user_config = HashWithIndifferentAccess.new(
+          YAML.load(
+            File.read( File.expand_path(File.join('config', 'stylin.yml'), app.root) )
+          )
+        ).symbolize_keys
+      else
+        user_config = {}
+      end
+
+      Stylin::APP_CONFIG = config.merge( user_config )
+
+      Stylin::APP_CONFIG[:stylesheets_path] = File.expand_path(APP_CONFIG[:stylesheets_path], app.root)
+      Stylin::APP_CONFIG[:html_styleguides_path] = File.expand_path(APP_CONFIG[:html_styleguides_path], app.root)
+    end
+
     config.generators do |g|
       g.test_framework      :rspec,        :fixture => false
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
